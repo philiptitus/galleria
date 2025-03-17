e5ceddc4bd35f6f90630d4c27eac374e8a769589
Philip <mrphilipowade@gmail.com>
2025-03-17 12:37:56 +0300

    deploying the web frontend to vercel

--- frontend/src/actions/notificationActions.js
+++ frontend/src/actions/notificationActions.js
@@ -43,7 +43,7 @@ export const createChat = (chat) => async (dispatch, getState) => {
         }
 
         const { data } = await axios.post(
-            `/api/notifications/chat/`,
+            `https://galleriaapi.duckdns.org/api/notifications/chat/`,
             chat,
             config
         )
@@ -93,7 +93,7 @@ export const deleteMessage = (messageId) => async(dispatch, getState) => {
         }
 
         const { data } = await axios.delete(
-            `/api/notifications/${messageId}/delete/`,
+            `https://galleriaapi.duckdns.org/api/notifications/${messageId}/delete/`,
             config
         )
         dispatch({
